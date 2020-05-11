19fd3490f1757ae8267dfe5ab7a468427966ad31
Julia Alencar <juliaalencar@hotmail.com>
2020-05-10 23:11:24 -0300

    retirada de valor total

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var bool = false;
 var tempo;
+var aux;
+var hospedes;
+var data_inicial;
+var data_final;
 console.log('conectou');
 const url = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72/"
 const card = document.getElementById('cards')
@@ -19,7 +22,6 @@ function cardData(data) {
 function renderCard(data) {
     const div = document.createElement('div')
     div.className = 'col-md-4'
-    if (bool=false){
     div.innerHTML = `
     <div class="card">
         <img class="card-img-top" src="${data.photo}" alt="${data.name}" style="">
@@ -31,23 +33,7 @@ function renderCard(data) {
     </div>
     `
     card.appendChild(div)
-    } else {
-        var total = data.price * tempo
-        div.innerHTML = `
-        <div class="card">
-            <img class="card-img-top" src="${data.photo}" alt="${data.name}" style="">
-            <div clas="card-body"  style="padding: 1em; border-radius: 30px" >
-              <h5 class="card-title title">${data.property_type}</h5>
-              <p class="card-text" style="height: 3em">${data.name}</p>
-              <p class="card-text">R$: <strong>${data.price}</strong>/noite</p>
-              <p class="card-text">Total: R$ ${total}</p>
-          </div>
-        </div>
-        `
-        card.appendChild(div) 
-    }
 }
-
 async function main(){
     dados = await fetchCards();
     if(dados[0]){
@@ -56,14 +42,14 @@ async function main(){
 }
 
 function botaoClick(){
-    var hospedes = document.getElementById('num_hospedes').value;
-    var data_inicial = new Date(document.getElementById('dia_inicial').value);
+    hospedes = document.getElementById('num_hospedes').value;
+    data_inicial = new Date(document.getElementById('dia_inicial').value);
     console.log('data inicial é ' + data_inicial);
-    var data_final = new Date(document.getElementById('dia_final').value);
+    data_final = new Date(document.getElementById('dia_final').value);
     console.log('data final é ' + data_final);
-    tempo = (data_final.getTime() - data_inicial.getTime()) / (1000 * 3600 * 24);
+    aux = ((data_final.getTime() - data_inicial.getTime()) / (1000 * 3600 * 24))
+    tempo = aux > 0 ? aux : "";
     console.log('tempo total da estadia: ' + tempo);
-    bool = true;
 }
 
 function valorTotal(tempo, valor){
