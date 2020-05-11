9651fbf9456e6652f544c989a344348a880dbb81
Julia Alencar <juliaalencar@hotmail.com>
2020-05-10 21:22:58 -0300

    carregando cards da API

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
 console.log('conectou');
-const baseAPI = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
-const cardsContainer = document.querySelector("#propriedades");
-let dados = [];
-
-async function fetchCards() {
-    return await fetch(baseAPI)
-      .then(async(r) => await r.json())
- }
-
-function renderCards(cards){
-    cardsContainer.innerHTML = "";
-    cards.map(renderCard);
+const url = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72/"
+  const card = document.getElementById('cards')
+fetch(url)
+    .then(response =>
+        response.json()
+    )
+    .then(data => {
+        cardData(data)
+    })
+    .catch(err => console.log(err))
+function cardData(data) {
+    card.innerHTML = "";
+    data.map(renderCard)
 }
-
-function renderCard(card){
-    const div = document.createElement("div");
-    div.style.width = "20rem";
-    div.style.margin = "2rem";
-    div.className = "card";
+function renderCard(data) {
+    const div = document.createElement('div')
+    div.className = 'col-md-4'
     div.innerHTML = `
-        <img src="${card.photo}" class="card-img-top" alt=""/>
-        <div class="card-body">
-            <h5 class="card-title">${card.name}</h5>
-            <p class="card-text">
-                Tipo: ${card.property_type}
-            </p>
-            <p class="card-text">
-                Tipo: R$ ${card.price},00
-            </p>
-        </div>
-        `;
-    cardsContainer.appendChild(div);
+    <div class="card">
+        <img class="card-img-top" src="${data.photo}" alt="${data.name}" style="">
+        <div clas="card-body"  style="padding: 1em; border-radius: 20px" >
+          <h5 class="card-title title">${data.property_type}</h5>
+          <p class="card-text" style="height: 3em">${data.name}</p>
+          <p class="card-text">R$: <strong>${data.price}</strong>/noite</p>
+      </div>
+    </div>
+    `
+    card.appendChild(div)
 }
 
 async function main(){
